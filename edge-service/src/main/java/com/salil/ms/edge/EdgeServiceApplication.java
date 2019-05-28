404d5075618f2f6b7f7e8fc2d3b11d9eb36a59df
salil <salil@xyz>
2019-05-28 09:29:01 +0100

    Adding mvn files

--- edge-service/src/main/java/com/salil/ms/edge/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/salil/ms/edge/EdgeServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,4 +13,11 @@ public class EdgeServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EdgeServiceApplication.class, args);
     }
+
+    @Bean
+    public SimpleFilter simpleFilter() {
+        return new SimpleFilter();
+    }
 }
+
+
